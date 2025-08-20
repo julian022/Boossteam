972ba367d7965b37599a6d3e5ce7112d5926771b
julian022 <146751300+julian022@users.noreply.github.com>
2025-08-19 23:01:16 -0500

    primer coomit proyect

---
+++ src/test/java/co/edu/uniajc/CRM/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniajc.CRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
